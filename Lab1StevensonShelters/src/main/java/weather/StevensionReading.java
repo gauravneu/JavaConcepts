feb6a77ce28f33cde69fb8c7e897ac83a64b76f1
gauravneu <99050945+gauravneu@users.noreply.github.com>
2023-05-19 23:22:04 -0400

    Lab1 updated

--- Lab1StevensonShelters/src/main/java/weather/StevensionReading.java
+++ Lab1StevensonShelters/src/main/java/weather/StevensionReading.java
@@ -13,6 +13,14 @@ public final class StevensionReading implements WeatherReading {
     private final double c7 = 0.002211732;
     private final double c8 = 0.00072546;
     private final double c9 = -0.000003582;
+    private final double a1 = 100.0;
+    private final double a2 = 5.0;
+    private final double a3 = 35.74;
+    private final double a4 = 0.6215;
+    private final double a5 = 35.75;
+    private final double a6 = 0.4275;
+    private final double a7 = 0.16;
+    private final int pow2 = 2;
     private final double heatIndex;
     private final double relativeHumidity;
     private final double windChill;
@@ -23,28 +31,38 @@ public final class StevensionReading implements WeatherReading {
 
     public StevensionReading(double totalRain, double dewPoint, double temperature, double windSpeed) throws IllegalArgumentException {
         if (dewPoint > temperature || windSpeed < 0 || totalRain < 0) {
-            throw new IllegalArgumentException("Illegal Input Values");
+            throw new IllegalArgumentException("IllegalArgumentException");
         }
         this.totalRain = totalRain;
         this.dewPoint = dewPoint;
         this.temperature = temperature;
         this.windSpeed = windSpeed;
 
-        this.relativeHumidity = 100 + 5 * (this.dewPoint - this.temperature);
-        this.heatIndex = c1 +
+        this. relativeHumidity = calcRelativeHumidity(this.dewPoint, this.temperature);
+        this.heatIndex = calcHeatIndex(this.temperature, this.relativeHumidity);
+        this.windChill = calcWindChill(this.temperature, this.windSpeed);
+
+    }
+    private double calcRelativeHumidity(double dewPoint, double temperature){
+        return a1 + a2 * (this.dewPoint - this.temperature);
+    }
+
+    private double calcHeatIndex(double temperature, double relativeHumidity){
+        return c1 +
                 c2 * this.temperature +
                 c3 * this.relativeHumidity +
                 c4 * this.temperature * this.relativeHumidity +
-                c5 * Math.pow(this.temperature, 2) +
-                c6 * Math.pow(this.relativeHumidity, 2) +
-                c7 * Math.pow(this.temperature, 2) * this.relativeHumidity +
-                c8 * this.temperature * Math.pow(this.relativeHumidity, 2) +
-                c9 * Math.pow(this.temperature, 2) * Math.pow(this.relativeHumidity, 2);
+                c5 * Math.pow(this.temperature, pow2) +
+                c6 * Math.pow(this.relativeHumidity, pow2) +
+                c7 * Math.pow(this.temperature, pow2) * this.relativeHumidity +
+                c8 * this.temperature * Math.pow(this.relativeHumidity, pow2) +
+                c9 * Math.pow(this.temperature, pow2) * Math.pow(this.relativeHumidity, pow2);
+    }
 
-        this.windChill = 35.74 + 0.6215 * this.temperature -
-                35.75 * Math.pow(this.windSpeed, 0.16) +
-                0.4275 * this.temperature * Math.pow(this.windSpeed, 0.16);
-
+    private double calcWindChill(double temperature, double windSpeed){
+        return a3 + a4 * this.temperature -
+                a5 * Math.pow(this.windSpeed, a7) +
+                a6 * this.temperature * Math.pow(this.windSpeed, a7);
     }
 
     @Override
@@ -70,52 +88,52 @@ public final class StevensionReading implements WeatherReading {
 
     @Override
     public int getWindSpeed() {
-        return (int) windSpeed;
+        return (int) this.windSpeed;
     }
 
     @Override
     public int getTotalRain() {
-        return (int) totalRain;
+        return (int) this.totalRain;
     }
 
 
     @Override
     public int getRelativeHumidity() {
-        return (int) relativeHumidity;
+        return (int) this.relativeHumidity;
     }
 
     @Override
     public int getHeatIndex() {
-        return (int) heatIndex;
+        return (int) this.heatIndex;
     }
 
     @Override
     public int getWindChill() {
-        return (int) windChill;
+        return (int) this.windChill;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || this.getClass() != o.getClass()) return false;
         StevensionReading that = (StevensionReading) o;
-        return Double.compare(that.heatIndex, heatIndex) == 0
-                && Double.compare(that.relativeHumidity, relativeHumidity) == 0
-                && Double.compare(that.windChill, windChill) == 0
-                && Double.compare(that.totalRain, totalRain) == 0
-                && Double.compare(that.dewPoint, dewPoint) == 0
-                && Double.compare(that.temperature, temperature) == 0
-                && Double.compare(that.windSpeed, windSpeed) == 0;
+        return Double.compare(that.heatIndex, this.heatIndex) == 0
+                && Double.compare(that.relativeHumidity, this.relativeHumidity) == 0
+                && Double.compare(that.windChill, this.windChill) == 0
+                && Double.compare(that.totalRain, this.totalRain) == 0
+                && Double.compare(that.dewPoint, this.dewPoint) == 0
+                && Double.compare(that.temperature, this.temperature) == 0
+                && Double.compare(that.windSpeed, this.windSpeed) == 0;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(heatIndex,
-                relativeHumidity,
-                windChill,
-                totalRain,
-                dewPoint,
-                temperature,
-                windSpeed);
+        return Objects.hash(this.heatIndex,
+                this.relativeHumidity,
+                this.windChill,
+                this.totalRain,
+                this.dewPoint,
+                this.temperature,
+                this.windSpeed);
     }
 }
--- Lab1StevensonShelters/src/test/java/weather/StevensionReadingTest.java
+++ Lab1StevensonShelters/src/test/java/weather/StevensionReadingTest.java
@@ -1,53 +1,55 @@
 package weather;
 
-import junit.framework.TestCase;
-import org.junit.AfterClass;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class StevensionReadingTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+
+public class StevensionReadingTest {
 
     StevensionReading sReading;
     @Before
     public void setUp() throws Exception {
         sReading = new StevensionReading(1.5,2,3,4);
     }
-//    @AfterClass
-//    public void tearDown() throws Exception {
-//        System.out.println("All Test Completed");
-//    }
+
 
     @Test
     public void testTestToString() {
         assertEquals("Reading: T = 3 D = 2 v = 4 rain = 1", sReading.toString());
     }
-
+    @Test
     public void testGetTemperature() {
         assertEquals(3,sReading.getTemperature());
     }
-
+    @Test
     public void testGetDewPoint() {
         assertEquals(2,sReading.getDewPoint());
     }
-
+    @Test
     public void testGetWindSpeed() {
         assertEquals(4,sReading.getWindSpeed());
     }
-
+    @Test
     public void testGetTotalRain() {
         assertEquals(1,sReading.getTotalRain());
     }
-
+    @Test
     public void testGetRelativeHumidity() {
         assertEquals(95,sReading.getRelativeHumidity());
     }
-
+    @Test
     public void testGetHeatIndex() {
         assertEquals(49,sReading.getHeatIndex());
     }
-
+    @Test
     public void testGetWindChill() {
         assertEquals(-5,sReading.getWindChill());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testDewPointMoreThanTemp(){
+        sReading = new StevensionReading(1,2,1,1);
+    }
 }
