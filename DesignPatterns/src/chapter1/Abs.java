a8e25f083fe0e166968693cf47764338066cddf2
gauravneu <99050945+gauravneu@users.noreply.github.com>
2023-07-07 22:59:12 +0530

    Added Head First

---
+++ DesignPatterns/src/chapter1/Abs.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public abstract class Abs {
+
+    public int he(){
+        return 1;
+    };
+
+}
---
+++ DesignPatterns/src/chapter1/AbsC.java
@@ -0,0 +1,29 @@
+package chapter1;
+
+public class AbsC extends Abs{
+    private final boolean isOn;
+
+    {
+        isOn = true;
+
+    }
+    //
+//    public AbsC(boolean isOn) {
+//        this.isOn = isOn;
+//    }
+}
+
+class Demo{
+    public static void main(String[] args) {
+
+
+        AbsC a1 = new AbsC();
+        Abs a2 = new Abs() {
+            @Override
+            public int he() {
+                return super.he();
+            }
+        };
+        System.out.println(a2.getClass());
+    }
+}
